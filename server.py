1a7bbd4b175ad6cb9ce86a56bdfeac3dc07ee54c
Camilo Gonzalez <camilo@localhost.localdomain>
2016-03-07 09:16:32 -0500

    sockets is now initlized in specific network interface

--- server.py
+++ server.py
@@ -1,7 +1,13 @@
 import socket
+import fcntl
+import struct
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', ifname[:15]))[20:24])
 
 s = socket.socket()
-host = socket.gethostname()
+host = get_ip_address('wlan0')
 port = 7600
 s.bind((host, port))
 
